5d25dfaf5a42e4fdf1bb9f40be8a3ea4ec9192f2
Evelyn Heredia <evefhrd@gmail.com>
2022-06-28 17:47:16 -0300

    TG-test: super admin CRUD tests finished

--- test/specs/super.admin.spec.js
+++ test/specs/super.admin.spec.js
@@ -1,8 +1,8 @@
 const Header = require('../pageobjects/header');
 const Sidebar = require('../pageobjects/sidebar');
 const Footer = require('../pageobjects/footer');
-const SuperAdminHomePage = require('../pageobjects/sa.list.page');
-const SuperAdminForm = require('../pageobjects/sa.form.page');
+const ListPage = require('../pageobjects/list.page');
+const FormPage = require('../pageobjects/form.page');
 
 describe('Super Admins CRUD path testing', () => {
   beforeAll('Open browser', () => {
@@ -28,91 +28,111 @@ describe('Super Admins CRUD path testing', () => {
   it('Title: Super Admins should be displayed', async () => {
     await expect(Header.superAdmins).toBeDisplayed();
     Header.superAdmins.click();
-    await expect(SuperAdminHomePage.saTitle).toBeDisplayed();
-    await expect(SuperAdminHomePage.saTitle).toHaveText('Super Admins');
+    await expect(ListPage.saTitle).toBeDisplayed();
+    await expect(ListPage.saTitle).toHaveText('Super Admins');
   });
 
   it('List table should be displayed', async () => {
-    await SuperAdminHomePage.listDisplayed();
+    await ListPage.listDisplayed();
   });
 
   it('Edit button should be displayed', async () => {
-    await expect(SuperAdminHomePage.editBtn).toBeDisplayed();
-    await expect(SuperAdminHomePage.editBtn).toBeClickable();
+    await expect(ListPage.editBtn).toBeDisplayed();
+    await expect(ListPage.editBtn).toBeClickable();
   });
 
   it('Delete button should be displayed', async () => {
-    await expect(SuperAdminHomePage.deleteBtn).toBeDisplayed();
-    await expect(SuperAdminHomePage.deleteBtn).toBeClickable();
+    await expect(ListPage.deleteBtn).toBeDisplayed();
+    await expect(ListPage.deleteBtn).toBeClickable();
   });
 
   it('Create button should be displayed', async () => {
-    await expect(SuperAdminHomePage.createSaBtn).toBeDisplayed();
-    await expect(SuperAdminHomePage.createSaBtn).toBeClickable();
-    await expect(SuperAdminHomePage.createSaBtn).toHaveText('Create Super Admin');
-    await SuperAdminHomePage.createSaBtn.click();
+    await expect(ListPage.createSaBtn).toBeDisplayed();
+    await expect(ListPage.createSaBtn).toBeClickable();
+    await expect(ListPage.createSaBtn).toHaveText('Create Super Admin');
+    await ListPage.createSaBtn.click();
   });
 
   it('Title in the tab to create a new SA should be displayed', async () => {
-    await expect(SuperAdminForm.saTitle).toBeDisplayed();
-    await expect(SuperAdminForm.saTitle).toHaveText('Super Admins');
+    await expect(FormPage.saTitle).toBeDisplayed();
+    await expect(FormPage.saTitle).toHaveText('Super Admins');
   });
 
   it('Form to create a new SA should be displayed', async () => {
-    await expect(SuperAdminForm.form).toBeDisplayed();
-    await expect(SuperAdminForm.form).toHaveChildren(6);
+    await expect(FormPage.form).toBeDisplayed();
+    await expect(FormPage.form).toHaveChildren(6);
   });
 
   it('Back button should work properly', async () => {
-    await expect(SuperAdminForm.backBtn).toBeDisplayed();
-    await expect(SuperAdminForm.backBtn).toBeClickable();
-    await SuperAdminForm.backBtn.click();
+    await expect(FormPage.backBtn).toBeDisplayed();
+    await expect(FormPage.backBtn).toBeClickable();
+    await FormPage.backBtn.click();
   });
 
-  it('A new Super Admin should NOT be created', async () => {
-    await SuperAdminHomePage.createSaBtn.click();
-    await expect(browser).toHaveUrl('https://balti-trackgenix-app.vercel.app/super-admins/form');
-    await SuperAdminForm.errorInputsMsgs;
-    await SuperAdminForm.backBtn.click();
+  it('A new Super Admin should NOT be created with empty fields', async () => {
+    await ListPage.createSaBtn.click();
+    await FormPage.errorInputsMsgs;
+    await FormPage.backBtn.click();
   });
 
-  /* This tests were comented as the super admin form has been modified by devs
-it('A new Super Admin should be created', async () => {
-    await expect(SuperAdminHomePage.createSaBtn).toBeDisplayed();
-    await expect(SuperAdminHomePage.createSaBtn).toBeClickable();
-    await SuperAdminHomePage.createSaBtn.click();
-    await SuperAdminForm.newSa('Evelyn', 'Heredia', 'evefhrd@hotmail.com', '12345678p');
-});
+  it('A new Super Admin should be created', async () => {
+      await expect(ListPage.createSaBtn).toBeDisplayed();
+      await expect(ListPage.createSaBtn).toBeClickable();
+      await ListPage.createSaBtn.click();
+      await FormPage.newSa('Evelyn', 'Heredia', 'evefhrd@hotmail.com', '12345678p');
+    });
 
-it('Success message should be displayed', async () => {
-    await expect(saFormPage.saCreatedModal).toBeDisplayed();
-    await expect(saFormPage.saCreatedModal).toHaveText('');
-});
-
-it('OK button should close success message.', async () => {
-    await expect(saFormPage.modalOkBtn).toBeDisplayed();
-    await expect(saFormPage.modalOkBtn).toHaveText('OK');
-    await expect(saFormPage.modalOkBtn).toBeClickable();
-    await saFormPage.modalOkBtn.click();
-});
-*/
-it('Super Admin should not be deleted', async () => {
-  await SuperAdminHomePage.deleteCancelFunction();
-});
+//This path should be followed when master will be updated
 /*
-it('Super Admin should be deleted', async () => {
-    await SuperAdminHomePage.deleteAcceptFunction();
-});
+  it('Success message should be displayed', async () => {
+      await expect(saFormPage.saCreatedModal).toBeDisplayed();
+      await expect(saFormPage.saCreatedModal).toHaveText('');
+  });
+*/
+  it('OK button should close success message.', async () => {
+      await expect(FormPage.modalOkBtn).toBeDisplayed();
+      await expect(FormPage.modalOkBtn).toHaveText('OK');
+      await expect(FormPage.modalOkBtn).toBeClickable();
+      await FormPage.modalOkBtn.click();
+  });
 
-it('Successful deletion message should be displayed', async () => {
-    await expect(SuperAdminHomePage.modalSuccessDelete).toBeDisplayed();
-    await expect(SuperAdminHomePage.modalSuccessDelete).toHaveText('Super Admin deleted');
-});
+  it('Super Admin should not be deleted', async () => {
+    await ListPage.deleteCancelFunction();
+  });
+/* Delete an item
+  it('Super Admin should be deleted', async () => {
+      await ListPage.deleteAcceptFunction();
+  });
 
-it('OK button should close successful deletion message.', async () => {
-    await expect(SuperAdminHomePage.modalSuccessDelete).toBeDisplayed();
-    await expect(SuperAdminHomePage.modalSuccessDelete).toHaveText('OK');
-    await expect(SuperAdminHomePage.modalSuccessDelete).toBeClickable();
-    await SuperAdminHomePage.modalSuccessDelete.click();
-});*/
+  it('Successful deletion message should be displayed', async () => {
+      await expect(ListPage.modalSuccessDelete).toBeDisplayed();
+      await expect(ListPage.modalSuccessDelete).toHaveText('Super Admin deleted');
+  });
+
+  it('OK button should close successful deletion message.', async () => {
+      await expect(ListPage.modalSuccessDelete).toBeDisplayed();
+      await expect(ListPage.modalSuccessDelete).toHaveText('OK');
+      await expect(ListPage.modalSuccessDelete).toBeClickable();
+      await ListPage.modalSuccessDelete.click();
+  });
+*/
+  it('Super Admin should not be edited', async () => {
+    await expect(ListPage.editBtn).toBeDisplayed();
+    await expect(ListPage.editBtn).toBeClickable();
+    await ListPage.editBtn.click();
+    await expect(FormPage.backBtn).toBeDisplayed();
+    await expect(FormPage.backBtn).toBeClickable();
+    await FormPage.backBtn.click();
+    await expect(browser).toHaveUrl('https://balti-trackgenix-app.vercel.app/super-admins');
+  });
+
+  it('Super Admin should be edited', async () => {
+    await expect(ListPage.editBtn).toBeDisplayed();
+    await expect(ListPage.editBtn).toBeClickable();
+    await ListPage.editBtn.click();
+    await FormPage.newSa('Evelyn', 'Updated', 'evefhrd@hotmail.com', '12345678p');
+    await expect(FormPage.modalOkBtn).toBeDisplayed();
+    await expect(FormPage.modalOkBtn).toBeClickable();
+    await FormPage.modalOkBtn.click();
+  });
 });
