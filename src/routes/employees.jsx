f10b8705d2aa2b48083da236d3f2739e58f30ace
Nicolas Micheletti <nicolasamicheletti@gmail.com>
2022-06-28 16:45:51 -0300

    TG-92: employee routes modified

--- src/routes/employees.jsx
+++ src/routes/employees.jsx
@@ -1,23 +1,24 @@
 import React, { Suspense, lazy } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Switch, Redirect, useRouteMatch } from 'react-router-dom';
 import Loading from 'Components/Shared/Loading';
+import PrivateRoute from './PrivateRoute';
+
 const EmployeeWorkedHours = lazy(() => import('Components/Employee/WorkedHours'));
 const EmployeeProfile = lazy(() => import('Components/Employee/UserProfile/index'));
 const EmployeeHome = lazy(() => import('Components/Employee/Home'));
 
 const employeeRoutes = () => {
+  const { url } = useRouteMatch();
   return (
-    <Router>
-      <Suspense fallback={<Loading />}>
-        <Switch>
-          <Route exact path={'/employee/home'} component={EmployeeHome} />
-          <Route exact path={'/employee/:id'} />
-          <Route exact path={'/employee/projects/:id'} />
-          <Route exact path={'/employee/profile/:id'} component={EmployeeProfile} />
-          <Route exact path={'/employee/workedhours/:id'} component={EmployeeWorkedHours} />
-        </Switch>
-      </Suspense>
-    </Router>
+    <Suspense fallback={<Loading />}>
+      <Switch>
+        <PrivateRoute exact path={'/employee/home'} component={EmployeeHome} />
+        <PrivateRoute exact path={'/employee/:id'} />
+        <PrivateRoute exact path={'/employee/projects/:id'} />
+        <PrivateRoute exact path={'/employee/profile/:id'} component={EmployeeProfile} />
+        <PrivateRoute exact path={'/employee/workedhours/:id'} component={EmployeeWorkedHours} />
+      </Switch>
+    </Suspense>
   );
 };
 
