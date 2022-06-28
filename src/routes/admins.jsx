3753d53f1bac176f260f04e2f1216b2dabfdc3e7
Nicolas Micheletti <nicolasamicheletti@gmail.com>
2022-06-28 16:39:35 -0300

    Admin route created and index modified

---
+++ src/routes/admins.jsx
@@ -0,0 +1,43 @@
+import React, { lazy, Suspense } from 'react';
+import { Switch, Redirect, useRouteMatch } from 'react-router-dom';
+import PrivateRoute from 'Routes/PrivateRoute';
+import Layout from 'Components/Layout';
+
+const SuperAdmins = lazy(() => import('Components/SuperAdmin/SuperAdmins'));
+const Admins = lazy(() => import('Components/SuperAdmin/Admins'));
+const Employees = lazy(() => import('Components/SuperAdmin/Employees'));
+const Tasks = lazy(() => import('Components/SuperAdmin/Tasks'));
+const Projects = lazy(() => import('Components/SuperAdmin/Projects'));
+const TimeSheets = lazy(() => import('Components/SuperAdmin/TimeSheets'));
+
+const adminRoutes = [
+  { path: '/super-admin/admins', name: 'Admins' },
+  { path: '/super-admin/super-admins', name: 'Super-admins' },
+  { path: '/super-admin/employees', name: 'Employees' },
+  { path: '/super-admin/projects', name: 'Projects' },
+  { path: '/super-admin/time-sheets', name: 'Time-sheets' },
+  { path: '/super-admin/tasks', name: 'Tasks' }
+];
+
+const AdminRoutes = () => {
+  const { url } = useRouteMatch();
+
+  'Admin routes:', AdminRoutes;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Layout routes={adminRoutes}>
+        <Switch>
+          <PrivateRoute path={`${url}/super-admins`} component={SuperAdmins} />
+          <PrivateRoute path={`${url}/admins`} component={Admins} />
+          <PrivateRoute path={`${url}/employees`} component={Employees} />
+          <PrivateRoute path={`${url}/projects`} component={Projects} />
+          <PrivateRoute path={`${url}/time-sheets`} component={TimeSheets} />
+          <PrivateRoute path={`${url}/tasks`} component={Tasks} />
+          <Redirect to={`${url}/admins`} />
+        </Switch>
+      </Layout>
+    </Suspense>
+  );
+};
+
+export default AdminRoutes;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,5 +1,5 @@
 import React, { Suspense, lazy } from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Layout from 'Components/Layout';
 import Loading from 'Components/Shared/Loading';
 
@@ -22,43 +22,59 @@ const EmployeeProfile = lazy(() => import('Components/Employee/UserProfile/index
 const EmployeeHome = lazy(() => import('Components/Employee/Home'));
 const EmployeeWorkedHours = lazy(() => import('Components/Employee/WorkedHours'));*/
 
+/*const Routes = () => {
+  return (
+    <Router>
+    <Layout>
+    <Suspense fallback={<Loading />}>
+    <Switch>
+    <Route exact path={'/'} component={Home} />
+    <Route exact path={'/admins'} component={Admins} />
+    <Route exact path={'/admins/form'} component={AdminsForm} />
+    <Route exact path={'/admins/form/:id'} component={AdminsForm} />
+    <Route exact path={'/super-admins'} component={SuperAdmins} />
+    <Route exact path={'/super-admins/form'} component={SuperAdminsForm} />
+    <Route exact path={'/super-admins/form/:id'} component={SuperAdminsForm} />
+    <Route exact path={'/employees'} component={Employees} />
+    <Route exact path={'/employees/form'} component={EmployeesForm} />
+    <Route exact path={'/employees/form/:id'} component={EmployeesForm} />
+    <Route exact path={'/projects'} component={Projects} />
+    <Route exact path={'/projects/form'} component={ProjectsForm} />
+    <Route exact path={'/projects/form/:id'} component={ProjectsForm} />
+    <Route exact path={'/tasks'} component={TasksList} />
+    <Route exact path={'/tasks/form/'} component={TasksForm} />
+    <Route exact path={'/tasks/form/:id'} component={TasksForm} />
+    <Route exact path={'/timesheets'} component={TimeSheetsList} />
+    <Route exact path={'/timesheets/form/'} component={TimeSheetsForm} />
+    <Route exact path={'/timesheets/form/:id'} component={TimeSheetsForm} />
+    <Route exact path={'/employee'} component={EmployeeHome} />
+    <Route exact path={'/employee/projects'} component={EmployeeProjects} />
+    <Route exact path={'/employee/profile'} component={EmployeeProfile} />
+    <Route exact path={'/employee/workedhours'} component={EmployeeWorkedHours} />
+    </Switch>
+    </Suspense>
+    </Layout>
+    </Router>
+    );
+  };
+  */
+
 const AdminRoutes = lazy(() => import('Routes/admin'));
 const EmployeeRoutes = lazy(() => import('Routes/employee'));
 const AuthRoutes = lazy(() => import('Routes/auth'));
 
 const Routes = () => {
   return (
-    <Router>
-      <Layout>
-        <Suspense fallback={<Loading />}>
-          <Switch>
-            <Route exact path={'/'} component={Home} />
-            <Route exact path={'/admins'} component={Admins} />
-            <Route exact path={'/admins/form'} component={AdminsForm} />
-            <Route exact path={'/admins/form/:id'} component={AdminsForm} />
-            <Route exact path={'/super-admins'} component={SuperAdmins} />
-            <Route exact path={'/super-admins/form'} component={SuperAdminsForm} />
-            <Route exact path={'/super-admins/form/:id'} component={SuperAdminsForm} />
-            <Route exact path={'/employees'} component={Employees} />
-            <Route exact path={'/employees/form'} component={EmployeesForm} />
-            <Route exact path={'/employees/form/:id'} component={EmployeesForm} />
-            <Route exact path={'/projects'} component={Projects} />
-            <Route exact path={'/projects/form'} component={ProjectsForm} />
-            <Route exact path={'/projects/form/:id'} component={ProjectsForm} />
-            <Route exact path={'/tasks'} component={TasksList} />
-            <Route exact path={'/tasks/form/'} component={TasksForm} />
-            <Route exact path={'/tasks/form/:id'} component={TasksForm} />
-            <Route exact path={'/timesheets'} component={TimeSheetsList} />
-            <Route exact path={'/timesheets/form/'} component={TimeSheetsForm} />
-            <Route exact path={'/timesheets/form/:id'} component={TimeSheetsForm} />
-            <Route exact path={'/employee'} component={EmployeeHome} />
-            <Route exact path={'/employee/projects'} component={EmployeeProjects} />
-            <Route exact path={'/employee/profile'} component={EmployeeProfile} />
-            <Route exact path={'/employee/workedhours'} component={EmployeeWorkedHours} />
-          </Switch>
-        </Suspense>
-      </Layout>
-    </Router>
+    <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route path="/admin" component={AdminRoutes} />
+          <Route path="/employee" component={EmployeeRoutes} />
+          <Route path="/auth" component={AuthRoutes} />
+          <Redirect to="/auth" />
+        </Switch>
+      </Suspense>
+    </BrowserRouter>
   );
 };
 
