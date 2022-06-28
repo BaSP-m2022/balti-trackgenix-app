285e137e682f87bcf71c09670e9d65ded0293601
Evelyn Heredia <evefhrd@gmail.com>
2022-06-28 20:04:50 -0300

    TG-test: employees CRUD tests finished

--- test/pageobjects/list.page.js
+++ test/pageobjects/list.page.js
@@ -1,63 +1,70 @@
 class SuperAdminHomePage {
   get listTitle() {
-      return $(' #root > div > div > section > h2');
+    return $(' #root > div > div > section > h2');
   }
   get listTable() {
-      return $('.list_container__21Buw > table');
+    return $('.list_container__21Buw > table');
   }
   get listHeader() {
-      return $('.list_container__21Buw > table > thead > tr');
+    return $('.list_container__21Buw > table > thead > tr');
   }
   get listBody() {
-      return $('.list_container__21Buw > table > tbody');
+    return $('.list_container__21Buw > table > tbody');
   }
   get createSaBtn() {
-      return $('#root > div > div > section > div:nth-child(3) > a > button');
+    return $('#root > div > div > section > div:nth-child(3) > a > button');
   }
   get editBtn() {
-      return $('.list_container__21Buw > table > tbody > tr:nth-child(1) > td:nth-child(5) > a > button');
+    return $(
+      '.list_container__21Buw > table > tbody > tr:nth-child(1) > td:nth-child(5) > a > button'
+    );
   }
   get deleteBtn() {
-      return $('.list_container__21Buw > table > tbody > tr:nth-child(1) > td:nth-child(6) > span > button');
+    return $(
+      '.list_container__21Buw > table > tbody > tr:nth-child(1) > td:nth-child(6) > span > button'
+    );
   }
   get deleteModalQuestion() {
-      return $('#root > div > div > section > div.Modal_modalOverlay__1ukkt > div');
+    return $('#root > div > div > section > div.Modal_modalOverlay__1ukkt > div');
   }
   get deleteModalAcceptBtn() {
-      return $('.Button_btn__3cUFC.Button_btnBlock__36Rss');
+    return $('.Button_btn__3cUFC.Button_btnBlock__36Rss');
   }
   get deleteModalCancelBtn() {
-      return $('.Button_btn__3cUFC.Button_redBtn__z6CdP');
+    return $('.Button_btn__3cUFC.Button_redBtn__z6CdP');
   }
   get modalSuccessDelete() {
-      return $('.Modal_modalOverlay__1ukkt > div');
+    return $('.Modal_modalOverlay__1ukkt > div');
   }
 
-  async listDisplayed () {
-      await expect(this.listTable).toBeDisplayed();
-      await expect(this.listHeader).toBeDisplayed();
-      await expect(this.listHeader).toHaveChildren(6);
-      await expect(this.listHeader).toHaveTextContaining('First name');
-      await expect(this.listHeader).toHaveTextContaining('Last name');
-      await expect(this.listHeader).toHaveTextContaining('Email');
-      await expect(this.listHeader).toHaveTextContaining('is Active');
-      await expect(this.listHeader).toHaveTextContaining('Edit');
-      await expect(this.listHeader).toHaveTextContaining('Delete');
+  async listDisplayed() {
+    await expect(this.listTable).toBeDisplayed();
+    await expect(this.listHeader).toBeDisplayed();
+    await expect(this.listHeader).toHaveChildren(6);
+    await expect(this.listHeader).toHaveTextContaining('First name');
+    await expect(this.listHeader).toHaveTextContaining('Last name');
+    await expect(this.listHeader).toHaveTextContaining('Email');
+    await expect(this.listHeader).toHaveTextContaining('is Active');
+    await expect(this.listHeader).toHaveTextContaining('Edit');
+    await expect(this.listHeader).toHaveTextContaining('Delete');
   }
-  async deleteCancelFunction () {
-      await this.deleteBtn.click();
-      await expect(this.deleteModalQuestion).toBeDisplayed();
+  async deleteCancelFunction() {
+    await this.deleteBtn.click();
+    await expect(this.deleteModalQuestion).toBeDisplayed();
     await expect(this.deleteModalQuestion).toHaveTextContaining(
-      'Are you sure you want to delete this super admin?');
-      await expect(this.deleteModalCancelBtn).toBeDisplayed();
-      await this.deleteModalCancelBtn.click();
+      'Are you sure you want to delete this super admin?'
+    );
+    await expect(this.deleteModalCancelBtn).toBeDisplayed();
+    await this.deleteModalCancelBtn.click();
   }
-  async deleteAcceptFunction () {
-      await this.deleteBtn.click();
-      await expect(this.deleteModalQuestion).toBeDisplayed();
-      await expect(this.deleteModalQuestion).toBeHaveText('Are you sure you want to delete this super admin?');
-      await expect(this.deleteModalAcceptBtn).toBeClickable();
-      await this.deleteModalAcceptBtn.click();
+  async deleteAcceptFunction() {
+    await this.deleteBtn.click();
+    await expect(this.deleteModalQuestion).toBeDisplayed();
+    await expect(this.deleteModalQuestion).toBeHaveText(
+      'Are you sure you want to delete this super admin?'
+    );
+    await expect(this.deleteModalAcceptBtn).toBeClickable();
+    await this.deleteModalAcceptBtn.click();
   }
 }
 
--- test/specs/employees.spec.js
+++ test/specs/employees.spec.js
@@ -1,32 +1,116 @@
 const Header = require('../pageobjects/header');
 const Sidebar = require('../pageobjects/sidebar');
 const Footer = require('../pageobjects/footer');
+const ListPage = require('../pageobjects/list.page');
+const FormPage = require('../pageobjects/form.page');
+const Employee = require('../pageobjects/employee.page');
 
 describe('Employee CRUD path testing', () => {
-    beforeAll('Open browser', ()=> {
-        browser.url('https://balti-trackgenix-app.vercel.app/');
-    });
+  beforeAll('Open browser', () => {
+    browser.url('https://balti-trackgenix-app.vercel.app/');
+  });
 
-    it('Header should be displayed and interactive', async () => {
-        await Header.wholeHeaderDisplay();
-    });
+  it('Header should be displayed and interactive', async () => {
+    await Header.wholeHeaderDisplay();
+  });
 
-    it('TG logo in the header should redirect to homepage', async () => {
-        await Header.TGredirection();
-    });
+  it('TG logo in the header should redirect to homepage', async () => {
+    await Header.TGredirection();
+  });
 
-    it('Footer should be displayed and interactive', async () => {
-        await Footer.wholeFooterDisplay();
-    });
+  it('Footer should be displayed and interactive', async () => {
+    await Footer.wholeFooterDisplay();
+  });
 
-    it('Sidebar should be displayed and interactive', async () => {
-        await Sidebar.wholeSidebarDisplay();
-    });
+  it('Sidebar should be displayed and interactive', async () => {
+    await Sidebar.wholeSidebarDisplay();
+  });
 
-    it('Title: Employees should be displayed', async () => {
-        await expect(Header.employees).toBeDisplayed();
-        Header.employees.click();
-        //await expect(SuperAdminHomePage.saTitle).toBeDisplayed();
-        //await expect(SuperAdminHomePage.saTitle).toHaveText('Super Admins');
-    });
+  it('Title: Employees should be displayed', async () => {
+    await expect(Header.employees).toBeDisplayed();
+    Header.employees.click();
+    await expect(ListPage.listTitle).toBeDisplayed();
+    await expect(ListPage.listTitle).toHaveText('Employees');
+  });
+
+  it('List table should be displayed', async () => {
+    await Employee.listDisplayed();
+  });
+
+  it('Edit button should be displayed', async () => {
+    await expect(ListPage.editBtn).toBeDisplayed();
+    await expect(ListPage.editBtn).toBeClickable();
+  });
+
+  it('Delete button should be displayed', async () => {
+    await expect(ListPage.deleteBtn).toBeDisplayed();
+    await expect(ListPage.deleteBtn).toBeClickable();
+  });
+
+  it('Create button should be displayed', async () => {
+    await expect(ListPage.createSaBtn).toBeDisplayed();
+    await expect(ListPage.createSaBtn).toBeClickable();
+    await expect(ListPage.createSaBtn).toHaveText('Create Employee');
+    await ListPage.createSaBtn.click();
+  });
+
+  it('Title in the tab to create a new Employee should be displayed', async () => {
+    await expect(FormPage.title).toBeDisplayed();
+    await expect(FormPage.title).toHaveText('Employees');
+  });
+
+  it('Form to create a new Employee should be displayed', async () => {
+    await expect(FormPage.form).toBeDisplayed();
+    await expect(FormPage.form).toHaveChildren(7);
+  });
+
+  it('Back button should work properly', async () => {
+    await expect(FormPage.backBtn).toBeDisplayed();
+    await expect(FormPage.backBtn).toBeClickable();
+    await FormPage.backBtn.click();
+  });
+
+  it('A new Employee should NOT be created with empty fields', async () => {
+    await ListPage.createSaBtn.click();
+    await FormPage.errorInputsMsgs;
+    await FormPage.backBtn.click();
+  });
+
+  it('A new Employee should be created', async () => {
+    await expect(ListPage.createSaBtn).toBeDisplayed();
+    await expect(ListPage.createSaBtn).toBeClickable();
+    await ListPage.createSaBtn.click();
+    await Employee.newEmployee('Evelyn', 'Heredia', 'evefhrd@hotmail.com', '12345678p');
+  });
+
+  it('OK button should close success message.', async () => {
+    await expect(FormPage.modalOkBtn).toBeDisplayed();
+    await expect(FormPage.modalOkBtn).toHaveText('OK');
+    await expect(FormPage.modalOkBtn).toBeClickable();
+    await FormPage.modalOkBtn.click();
+  });
+
+  it('Employee should not be edited', async () => {
+    await expect(ListPage.editBtn).toBeDisplayed();
+    await expect(ListPage.editBtn).toBeClickable();
+    await ListPage.editBtn.click();
+    await expect(FormPage.backBtn).toBeDisplayed();
+    await expect(FormPage.backBtn).toBeClickable();
+    await FormPage.backBtn.click();
+    await expect(browser).toHaveUrl('https://balti-trackgenix-app.vercel.app/employees');
+  });
+
+  it('Employee should be edited', async () => {
+    await expect(ListPage.editBtn).toBeDisplayed();
+    await expect(ListPage.editBtn).toBeClickable();
+    await ListPage.editBtn.click();
+    await Employee.newEmployee('Evelyn', 'Updated', 'evefhrd@hotmail.com', '12345678p');
+    await expect(FormPage.modalOkBtn).toBeDisplayed();
+    await expect(FormPage.modalOkBtn).toBeClickable();
+    await FormPage.modalOkBtn.click();
+  });
+
+  it('Employee should not be deleted', async () => {
+    await Employee.deleteCancelFunction();
+  });
 });
